918fc8a2a5f9f853425b6fd8e73fd78148c09702
Jmara <jmara.0923@gmail.com>
2024-11-11 19:29:47 +0400

    Add print board

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,13 +1,24 @@
 class ConnectFour
-  attr_accessor :board
+  attr_accessor :board, :white_token, :black_token
 
   def initialize
-    @board = Array.new(6) { Array.new(7) }
-    @white_token = "\u26AA"
-    @black_token = "\u26AB"
+    @board = Array.new(6) { Array.new(7) { ' ' } }
+    @white_token = "\u25CF"
+    @black_token = "\u25CB"
   end
 
   def insert_token(token, column)
     @board[5][column - 1] = token
+  end
+
+  def print_board
+    puts "\nPick one from the column below\n\n"
+    print "    #{%w[1 2 3 4 5 6 7].join('   ')}\n"
+    @board.each_with_index do |row, index|
+      print "#{index + 1} "
+      puts row.map { |cell| "| #{cell} " }.join + '|'
+      print '  '
+      puts '-' * ((row.length * 4) + 1)
+    end
   end
 end
